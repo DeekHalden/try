e4c561c51ff8ccde9f92a8ea2982a156da327a1b
deekhalden <iliy4@ua.fm>
2016-11-01 12:05:12 +0200

    better

--- scripts/controllers/overview.js
+++ scripts/controllers/overview.js
@@ -94,7 +94,7 @@
                 time: '10.00',
                 description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                 image: 'http://photos.wikimapia.org/p/00/02/98/16/39_big.jpg',
-                alt: 'Kiev',
+                alt: 'Kiev,Dynamo stadium',
                 index: 1,
                 coords: [
                     50.23,
@@ -104,7 +104,7 @@
                 time: '15.00',
                 description: 'Fluidity convenience store nodality systema dolphin garage footage courier into hotdog 3D-printed plastic bomb car saturation point. Meta-nano-saturation point stimulate papier-mache rifle math-katana advert into. Pistol faded nodal point render-farm bridge math-savant table jeans 3D-printed. Wristwatch tube vehicle otaku assault order-flow pre. Motion decay computer assault pre-jeans artisanal sub-orbital shrine sprawl refrigerator systemic. Girl assassin Legba face forwards concrete dome alcohol youtube industrial grade city gang-space rain stimulate chrome. Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                 image: 'http://loveopium.ru/content/2013/05/fountains/18big.jpg',
-                alt: 'Dnipro',
+                alt: 'Dnipro, Arena',
                 index: 2,
                 coords: [
                     48.4647,
@@ -114,11 +114,12 @@
                 time: '16.00',
                 description: 'Marketing face forwards convenience store 8-bit alcohol corrupted saturation point military-grade jeans tiger-team sub-orbital. Corporation cartel garage RAF uplink disposable car BASE jump otaku hacker systema convenience store camera neon youtube. Towards assault tank-traps physical man denim city chrome wristwatch. Digital corrupted faded apophenia footage denim futurity city tiger-team Tokyo narrative tanto tank-traps claymore mine. Digital singularity construct-space warehouse assault motion DIY rifle pistol wristwatch. Refrigerator urban fetishism pistol into towards car numinous corporation DIY j-pop computer saturation point sign apophenia kanji. Free-market dome pistol-ware ablative Shibuya rifle neural order-flow Chiba physical refrigerator. Wristwatch saturation point courier BASE jump construct singularity pistol car carbon modem savant tattoo shanty town shrine systema skyscraper rain. Free-market refrigerator sunglasses shrine geodesic marketing tower dolphin. Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                 image: 'https://media-cdn.tripadvisor.com/media/photo-s/01/61/a7/d4/gondola-rides-at-the.jpg',
-                alt: 'Berdyansk',
+                alt: 'Аквапарк',
                 index: 3,
                 coords: [
-                    46.7584,
-                    36.8081
+                    46.7202,
+                    36.8231
+
                 ]
             }]
         }, {
@@ -127,7 +128,7 @@
                 time: '10.00',
                 description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                 image: 'http://photos.wikimapia.org/p/00/02/98/16/39_big.jpg',
-                alt: 'Stambul',
+                alt: 'Stambul, Stadium',
                 index: 1,
                 coords: [
                     41.015137, 28.979530
@@ -137,7 +138,7 @@
                 time: '15.00',
                 description: 'Fluidity convenience store nodality systema dolphin garage footage courier into hotdog 3D-printed plastic bomb car saturation point. Meta-nano-saturation point stimulate papier-mache rifle math-katana advert into. Pistol faded nodal point render-farm bridge math-savant table jeans 3D-printed. Wristwatch tube vehicle otaku assault order-flow pre. Motion decay computer assault pre-jeans artisanal sub-orbital shrine sprawl refrigerator systemic. Girl assassin Legba face forwards concrete dome alcohol youtube industrial grade city gang-space rain stimulate chrome. Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                 image: 'http://loveopium.ru/content/2013/05/fountains/18big.jpg',
-                alt: 'Sofia',
+                alt: 'Sofia University',
                 index: 2,
                 coords: [
                     42.6965,
@@ -147,7 +148,7 @@
                 time: '16.00',
                 description: 'Marketing face forwards convenience store 8-bit alcohol corrupted saturation point military-grade jeans tiger-team sub-orbital. Corporation cartel garage RAF uplink disposable car BASE jump otaku hacker systema convenience store camera neon youtube. Towards assault tank-traps physical man denim city chrome wristwatch. Digital corrupted faded apophenia footage denim futurity city tiger-team Tokyo narrative tanto tank-traps claymore mine. Digital singularity construct-space warehouse assault motion DIY rifle pistol wristwatch. Refrigerator urban fetishism pistol into towards car numinous corporation DIY j-pop computer saturation point sign apophenia kanji. Free-market dome pistol-ware ablative Shibuya rifle neural order-flow Chiba physical refrigerator. Wristwatch saturation point courier BASE jump construct singularity pistol car carbon modem savant tattoo shanty town shrine systema skyscraper rain. Free-market refrigerator sunglasses shrine geodesic marketing tower dolphin. Lorem ipsum dolor sit amet, consectetur adipisicing elit',
                 image: 'https://media-cdn.tripadvisor.com/media/photo-s/01/61/a7/d4/gondola-rides-at-the.jpg',
-                alt: 'Lviv',
+                alt: 'Lviv, Just Lviv It',
                 index: 3,
                 coords: [
                     49.8397,
@@ -158,24 +159,31 @@
 
 
         vm.locationMessage = '';
-        vm.venueType = 'cafe';
+
 
         function showInfo(day, location, venueType) {
             vm.showId = 0;
-            console.log(venueType);
-            return $http.get("https://api.foursquare.com/v2/venues/search?ll=" + vm.days[day - 1].locations[location - 1].coords[0] + "%2C" + vm.days[day - 1].locations[location - 1].coords[1] + "&client_id=" + clientID + "&client_secret=" + clientSecret + "&v=20151222&query=" + venueType + "")
-                .then(function(data) {
-                    
-                    // each new request retrieve info about  random  venue in list of venues which are near seacrch location
-                    if (data.data.response.venues.length>0) {
-                        var venues  = data.data.response.venues;
-                        var number = getRandomInRange(0, venues.length - 1)
-                        var id = venues[number].id;
 
-                        return $http.get("https://api.foursquare.com/v2/venues/" + id + "?client_id=" + clientID + "&client_secret=" + clientSecret + "&v=20151222")
+            var locs = vm.days[day - 1].locations[location - 1];
+            return httpGetId(locs)
+                .then(function(data) {
+
+
+                    if (data.data.response.venues.length > 0) {
+                        var venues = data.data.response.venues;
+
+                        // get rand
+                        // var number = getRandomInRange(0, venues.length - 1)
+
+
+                        var id = venues[0].id;
+
+                        return httpGetVenueById(id)
                             .then(function(data) {
                                 // console.log(data);
                                 var venue = data.data.response.venue;
+
+                                console.log(venue.bestPhoto.prefix + venue.bestPhoto.width + 'x' + venue.bestPhoto.height + venue.bestPhoto.suffix);
 
                                 if (venue.tips.groups[0].items[0]) {
                                     vm.locationMessage = '<h3>' + venue.name + '</h3>' + '<p>' + venue.tips.groups[0].items[0].text + '</p><p>Click<a href="' + venue.shortUrl + '" target="_blank"> here </a>to learn more from Foursquare.</p>';
@@ -187,9 +195,9 @@
                                 vm.showId = day + '' + location;
 
                             });
-                        
+
                     } else {
-                        vm.locationMessage = '<p>No '+ venueType+'\'s found. </p>';
+                        vm.locationMessage = '<p>No ' + venueType + '\'s found. </p>';
                         vm.showId = day + '' + location;
 
                     }
@@ -197,10 +205,48 @@
                 })
         }
 
+        vm.photos = [];
+
+
+        function httpGetId(loc) {
+            return $http.get("https://api.foursquare.com/v2/venues/search?ll=" + loc.coords[0] + "%2C" + loc.coords[1] + "&client_id=" + clientID + "&client_secret=" + clientSecret + "&v=20151222&query=" + loc.alt + "")
+        }
+
+        function httpGetVenueById(id) {
+            return $http.get("https://api.foursquare.com/v2/venues/" + id + "?client_id=" + clientID + "&client_secret=" + clientSecret + "&v=20151222")
+        }
+
+        function getPhotos(locs) {
+            angular.forEach(locs, function(loc) {
+                return httpGetId(loc)
+                    .then(function(data) {
+                        var id = data.data.response.venues[0].id;
+                        return httpGetVenueById(id)
+                            .then(function(data) {
+                                var venue = data.data.response.venue;
+                                var photo = venue.bestPhoto.prefix + venue.bestPhoto.width + 'x' + venue.bestPhoto.height + venue.bestPhoto.suffix;
+                                vm.photos.push(photo);
+                                console.log(vm.photos);
+                            })
+                    })
+            })
+        };
+        // vm.photos = [].push(vm.photo);
+        // console.log(vm.photos);
+
+
         function showMap(index) {
 
+            var locs = vm.days[index - 1].locations;
+
+            getPhotos(locs);
+
             vm.mapId = index;
-        }
+        };
+
+
+
+
 
         function getRandomInRange(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
